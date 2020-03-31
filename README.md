2b4f99f433257b3020abcb63a9d9ed699ec25608
Mete Balci <metebalci@gmail.com>
2020-03-31 11:35:19 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
+
 # fasta2png
-fasta na or aa sequence to png converter
+
+This program generates PNG images from nucleic acid (na) sequences in FASTA format.
+
+# Installation
+
+```
+pip install fasta2png
+```
+
+# Usage
+
+```
+fna2png --input <input_in_fasta_format> --output <output_filename>
+```
+
+There are various options to customize PNG output, see help `fna2png --help` for more info.
+
+# Example
+
+[NC_045512.2](https://www.ncbi.nlm.nih.gov/nuccore/NC_045512) is the SARS-CoV-2 (corona virus 2) complete genome sequenced by Chinese researchers in January 2020.
+
+NC_045512.2.fna file below is https://www.ncbi.nlm.nih.gov/nuccore/NC_045512.2?report=fasta&log$=seqview&format=text.
+
+```
+$ fna2png --input NC_045512.2.fna --output NC_045512.2.png --pixel-size 4 --aspect-ratio 3 2
+
+seqdesc: NC_045512.2 Severe acute respiratory syndrome coronavirus 2 isolate Wuhan-Hu-1, complete genome
+seqlen: 29903
+```
+
+![NC_045512.2.png](NC_045512.2.png)
---
+++ fasta2png/fna2png.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageDraw
+import math
+import argparse
+import sys
+
+
+def _main(in_file,
+        out_file,
+        pixel_size, 
+        aspect_width, 
+        aspect_height, 
+        bg_color, 
+        colortable):
+    seq = ''
+    with open(in_file, 'r') as f:
+        first_seq_found = False
+        while True:
+            line = f.readline()
+            # end of file ?
+            if len(line) == 0:
+                break
+            # description ?
+            elif line[0] == '>':
+                # this program uses only the first sequence in the file
+                if first_seq_found:
+                    break
+                print('seqdesc: %s' % line[1:].strip())
+                first_seq_found = True
+                continue
+            # comment ?
+            elif line[0] == ';':
+                continue
+            seq = seq + line.strip()
+    seq = seq.lower()
+    # u and t is same for the purpose of this program
+    seq = seq.replace('u', 't')
+    print('seqlen: %d' % len(seq))
+    im_unit = math.floor(math.sqrt(len(seq)/(aspect_width*aspect_height)))
+    w = im_unit * aspect_width
+    h = im_unit * aspect_height
+    while len(seq) > (w*h):
+        h = h + 1
+    im_width = w * pixel_size
+    im_height = h * pixel_size
+    im = Image.new('RGBA', (im_width, im_height), color=bg_color)
+    draw = ImageDraw.Draw(im)
+    curr = 0
+    for y in range(0, h):
+        for x in range(0, w):
+            if curr == len(seq):
+                break
+            base = seq[curr]
+            x0 = x * pixel_size
+            y0 = y * pixel_size
+            draw.rectangle([x0, 
+                y0, 
+                x0+pixel_size, 
+                y0+pixel_size],
+                fill=colortable.get(base, colortable['n']))
+            curr = curr + 1
+    del draw
+    im.save(out_file, 'PNG')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', required=True, help='input (fasta/fna) file name')
+    parser.add_argument('--output', required=True, help='output (rgba png) file name')
+    parser.add_argument('--pixel-size', type=int, default=1, help='size of a (nucleotid) base in final png in pixels, default is 1')
+    parser.add_argument('--aspect-ratio', type=int, nargs=2, default=[1, 1],
+            help='aspect ratio, provide as two integers representing width and height, default is 1 1')
+    parser.add_argument('--bg-color', 
+            type=int, 
+            nargs=4, 
+            default=[0, 0, 0, 255],
+            help='background color rgba, 4 integers, default is opaque black')
+    # visually distinct colors
+    # from https://mokole.com/palette.html
+    parser.add_argument('--color-a',
+            type=int, 
+            nargs=4, 
+            default=[0xff, 0, 0, 0xff],
+            help='color of base Adenine, 4 integers, default is opaque red')
+    parser.add_argument('--color-g',
+            type=int, 
+            nargs=4, 
+            default=[0, 0xff, 0, 0xff],
+            help='color of base Guanine, 4 integers, default is opaque lime')
+    parser.add_argument('--color-c',
+            type=int, 
+            nargs=4, 
+            default=[0, 0, 0xff, 0xff],
+            help='color of base Cytosine, 4 integers, default is opaque blue')
+    parser.add_argument('--color-t',
+            type=int, 
+            nargs=4, 
+            default=[0x87, 0xce, 0xfa, 0xff],
+            help='color of base Thymine (and Uracil), 4 integers, default is opaque light sky blue')
+    parser.add_argument('--color-n',
+            type=int, 
+            nargs=4, 
+            default=[0xff, 0xff, 0xff, 0xff],
+            help='color of any other code in the sequence, 4 integers, default is opaque white')
+    args = parser.parse_args()
+    colortable = {}
+    colortable['a'] = tuple(args.color_a)
+    colortable['g'] = tuple(args.color_g)
+    colortable['c'] = tuple(args.color_c)
+    colortable['t'] = tuple(args.color_t)
+    colortable['n'] = tuple(args.color_n)
+    _main(args.input,
+            args.output,
+            args.pixel_size,
+            args.aspect_ratio[0],
+            args.aspect_ratio[1],
+            tuple(args.bg_color),
+            colortable)
